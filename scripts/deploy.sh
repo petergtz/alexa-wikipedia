7bf302b5d2676b4045f734415e2f4d0cd13ed9c2
Peter Goetz <peter.gtz@gmail.com>
2018-08-07 22:35:26 +0200

    Make deploy script cross-region capable

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,12 +12,18 @@ ginkgo -r
 export SHA=$(git rev-parse --short HEAD)
 export APP_NAME=alexa-wikipedia-$SHA
 
-cf push --no-start -b https://github.com/cloudfoundry/go-buildpack.git $APP_NAME
-cf set-env $APP_NAME APPLICATION_ID $(lpass show Personal/Alexa-Wikipedia-Skill-Application-ID --password)
-cf restart $APP_NAME
+for region in 'ng' 'eu-de' 'eu-gb'; do
+    open https://login.$region.bluemix.net/UAALoginServerWAR/passcode
 
-export OLD_RELEASES=$(cf apps | grep alexa-wikipedia | grep -v $SHA | cut -f 1 -d ' ')
+    cf login -a api.$region.bluemix.net --sso -o $(lpass show Personal/alexa-org --notes) -s alexa
 
-for release in $OLD_RELEASES; do
-    cf delete -f $release
+    cf push --no-start -b https://github.com/cloudfoundry/go-buildpack.git $APP_NAME --hostname alexa-wikipedia
+    cf set-env $APP_NAME APPLICATION_ID $(lpass show Personal/Alexa-Wikipedia-Skill-Application-ID --password)
+    cf restart $APP_NAME
+
+    export OLD_RELEASES=$(cf apps | grep alexa-wikipedia | grep -v $SHA | cut -f 1 -d ' ')
+
+    for release in $OLD_RELEASES; do
+        cf delete -f $release
+    done
 done
