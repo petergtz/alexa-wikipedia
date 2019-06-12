1a74ac2c3ee428c82c7469603e96f2d48031a617
Peter Goetz <peter.gtz@gmail.com>
2019-06-12 15:04:12 +0200

    Update run-local-server.sh to work with current code

--- scripts/run-local-server.sh
+++ scripts/run-local-server.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -e
 
 . private/dev-application-id.sh
+. private/s3-credentials.sh
 
-export cert=private/certificate.pem
-export key=private/private-key.pem
+export PORT=4443
+export SKILL_USE_TLS=true
+export CERT=private/certificate.pem
+export KEY=private/private-key.pem
+export TABLE_NAME_OVERRIDE=TestAlexaWikipediaRequests
 fresh
