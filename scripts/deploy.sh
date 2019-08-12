9ac54eab5ee6e34fa9a48451ccf65f8834ab4e35
Peter Goetz <peter.gtz@gmail.com>
2019-08-12 12:01:05 +0200

    Update LPass paths

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,12 +16,12 @@ export APP_NAME=alexa-wikipedia-$SHA
 for region in 'eu-gb' 'ng' 'eu-de'; do
     open https://login.$region.bluemix.net/UAALoginServerWAR/passcode
 
-    cf login -a api.$region.bluemix.net --sso -o $(lpass show Personal/Alexa-Wikipedia-Skill --notes) -s alexa
+    cf login -a api.$region.bluemix.net --sso -o $(lpass show Personal\\api_keys/Alexa-Wikipedia-Skill --notes) -s alexa
 
     cf push --no-start -b https://github.com/cloudfoundry/go-buildpack.git $APP_NAME --hostname alexa-wikipedia
-    cf set-env $APP_NAME APPLICATION_ID $(lpass show Personal/Alexa-Wikipedia-Skill --password)
-    cf set-env $APP_NAME ACCESS_KEY_ID $(lpass show Personal/Alexa-Wikipedia-S3 --username)
-    cf set-env $APP_NAME SECRET_ACCESS_KEY $(lpass show Personal/Alexa-Wikipedia-S3 --password)
+    cf set-env $APP_NAME APPLICATION_ID $(lpass show Personal\\api_keys/Alexa-Wikipedia-Skill --password)
+    cf set-env $APP_NAME ACCESS_KEY_ID $(lpass show Personal\\api_keys/Alexa-Wikipedia-S3 --username)
+    cf set-env $APP_NAME SECRET_ACCESS_KEY $(lpass show Personal\\api_keys/Alexa-Wikipedia-S3 --password)
     cf restart $APP_NAME
 
     export OLD_RELEASES=$(cf apps | grep alexa-wikipedia | grep -v $SHA | cut -f 1 -d ' ')
