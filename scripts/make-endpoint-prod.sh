bfc7f218d90db1b7dbf4fd48bb2deb96d5d8553b
Peter Goetz <peter.gtz@gmail.com>
2019-10-03 23:45:39 +0200

    Deployment scripts: introduce aliases, make them more flexible

---
+++ scripts/make-endpoint-prod.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -ex
+
+cd $(dirname $0)/..
+
+for region in 'us-east-1' 'eu-west-1' 'ap-northeast-1'; do
+    sed -E -i "s/(arn:aws:lambda:$region:512841817041:function:AlexaWikipedia)(.*)/\1:prod/g" skill.json
+done
+
+ask diff --target skill
+
+while true; do
+    read -p "Deploy changes? " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit 1;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+ask deploy --force --target skill
+
+rm -rf hooks
--- scripts/publish-version.sh
+++ scripts/publish-version.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 cd $(dirname $0)/..
 
@@ -6,27 +6,16 @@ for region in 'us-east-1' 'eu-west-1' 'ap-northeast-1'; do
     output=$(aws --region $region lambda publish-version --function-name AlexaWikipedia)
     echo $output
     version=$(echo $output | jq -r .Version)
-    sed -E -i "s/(arn:aws:lambda:$region:512841817041:function:AlexaWikipedia)(:?[0-9]*)/\1:$version/g" skill.json
+    
+    aws --region $region lambda update-alias \
+      --function-name AlexaWikipedia \
+      --name prod \
+      --function-version $version
 
     aws --region $region lambda add-permission \
-      --function-name AlexaWikipedia:$version \
+      --function-name AlexaWikipedia:prod \
       --action lambda:invokeFunction \
       --principal alexa-appkit.amazon.com  \
       --statement-id $(date +%s) \
       --event-source-token amzn1.ask.skill.101a1dcb-ce70-4fd6-ae01-6a8803f727ff
 done
-
-ask diff --target skill
-
-while true; do
-    read -p "Deploy changes? " yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit 1;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-ask deploy --target skill
-
-rm -rf hooks
