cffc8ba719eb6da86860579c5d8056e50b8dabf1
Peter Goetz <peter.gtz@gmail.com>
2019-11-06 22:07:59 +0100

    Fix issue persistence test

--- github/issue_persistence_test.go
+++ github/issue_persistence_test.go
@@ -7,21 +7,25 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/petergtz/alexa-wikipedia/github"
-	"go.uber.org/zap"
+	"github.com/petergtz/go-alexa"
 )
+
+type noOpErrorReporter struct{}
+
+func (*noOpErrorReporter) ReportPanic(interface{}, *alexa.RequestEnvelope) {}
+func (*noOpErrorReporter) ReportError(error)                               {}
 
 var _ = Describe("GithubPersistence", func() {
 	It("can use an issue comment as persistence", func() {
 		token, e := ioutil.ReadFile("../private/github-access-token")
 		Expect(e).NotTo(HaveOccurred())
-		l, e := zap.NewDevelopment()
-		if e != nil {
-			panic(e)
-		}
-		defer l.Sync()
-		log := l.Sugar()
 
-		pp := github.NewGithubPersistence("petergtz", "alexa-wikipedia", 549126277, strings.TrimSpace(string(token)), log)
+		pp := github.NewGithubPersistence(
+			"petergtz",
+			"alexa-wikipedia",
+			549126277,
+			strings.TrimSpace(string(token)),
+			&noOpErrorReporter{})
 		pp.Persist([]string{"xyz"})
 	})
 })
